f8184310dcdc09055df32cf87120d1bfd4ca8c3d
mbnfo <mbnfox123@gmail.com>
2023-11-29 03:07:38 -0800

    ''

--- src/pages/Flashcards.js
+++ src/pages/Flashcards.js
@@ -1,6 +1,8 @@
 import React, { useContext, useState, useEffect } from 'react'
 import AuthContext from '../context/AuthContext';
 import './flashcards.css'
+import { Link } from 'react-router-dom'
+
 
 const Flashcards = ({match}) => {
     let flashpackId = match.params.id
